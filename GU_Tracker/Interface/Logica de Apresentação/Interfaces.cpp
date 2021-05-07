d09a3b5f6d78ec157f45a568c3a9d7804f6ca124
Nicollas Escobar Mosca <70383747+NicollasEMosca@users.noreply.github.com>
2021-05-07 17:44:30 -0300

    Add classe interface
    add na classe interface com somente a declaração dos metodos.

---
+++ GU_Tracker/Interface/Logica de Apresentação/Interfaces.cpp
@@ -0,0 +1,21 @@
+#include "Interfaces.h"
+
+void Interfaces::printPlayerData() {
+	// TODO - implement Interfaces::printPlayerData
+	throw "Not yet implemented";
+}
+
+void Interfaces::printCaminhoOtimo() {
+	// TODO - implement Interfaces::printCaminhoOtimo
+	throw "Not yet implemented";
+}
+
+void Interfaces::printCaminhosAlternativos() {
+	// TODO - implement Interfaces::printCaminhosAlternativos
+	throw "Not yet implemented";
+}
+
+void Interfaces::printJogoData() {
+	// TODO - implement Interfaces::printJogoData
+	throw "Not yet implemented";
+}
---
+++ GU_Tracker/Interface/Logica de Apresentação/Interfaces.h
@@ -0,0 +1,19 @@
+#ifndef INTERFACES_H
+#define INTERFACES_H
+
+class Interfaces {
+
+private:
+	int attribute;
+
+public:
+	void printPlayerData();
+
+	void printCaminhoOtimo();
+
+	void printCaminhosAlternativos();
+
+	void printJogoData();
+};
+
+#endif
